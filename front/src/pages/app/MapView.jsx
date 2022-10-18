9a36cc90b8d6d73328b5655967a36c025fda47e3
Oliver Torres <olivertorres1997@gmail.com>
2022-10-18 19:34:37 -0400

    routes

--- front/src/pages/app/MapView.jsx
+++ front/src/pages/app/MapView.jsx
@@ -1,14 +1,19 @@
-import React, {  useState } from "react";
+
+import React, { useState } from "react";
 import {
+  Marker,
   GoogleMap,
   useJsApiLoader,
-  Marker,
-  DirectionsService,
   DirectionsRenderer,
 } from "@react-google-maps/api";
 import Sidebar from "../../components/app/Sidebar";
 import logo from "../../logo.svg";
-import BotonMiUbicacion from "../../components/app/BotonMiUbicacion";
+// import BotonMiUbicacion from "../../components/app/BotonMiUbicacion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationCrosshairs } from "@fortawesome/free-solid-svg-icons";
+
+
+const location = <FontAwesomeIcon icon={faLocationCrosshairs} />;
 
 const containerStyle = {
   width: "100%",
@@ -20,14 +25,8 @@ const center = {
   lng: -62.71150054759027,
 };
 
-const miibucacion = {
-  lat: 8.351952125568946,
-  lng: -62.663044001632784,
-};
-const origen = {
-  lat: 8.297220178301329,
-  lng: -62.71150054759027,
-};
+
+
 
 const destino = {
   lat: 8.332488759816176,
@@ -35,30 +34,24 @@ const destino = {
 };
 
 function MapView() {
+  const [directionsResponse, setDirectionsResponse] = useState(null)
 
-  const [response, setResponse] = useState(null);
+  async function calculateRoute() {
+    const google = window.google;
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin: center,
+      destination: destino,
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    setDirectionsResponse(results)
 
-  const regresaDireccion = (response) => {
-    console.log(response);
-    if (response !== null) {
-      if (response.status) {
-        setResponse(response);
-      } else {
-        console.log("response: ", response);
-      }
-    }
-  };
+  }
 
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
-
-  const DirectionsServiceRuta = {
-    origin: origen,
-    destination: destino,
-    travelMode: "DRIVING",
-  };
 
   return isLoaded ? (
     <>
@@ -74,16 +67,26 @@ function MapView() {
         }}
        
       >
-        <>
-          <Marker position={miibucacion} draggable={true} />
-          <BotonMiUbicacion /> 
-          <DirectionsService
-            callback={regresaDireccion}
-            options={DirectionsServiceRuta}
-          />
-          {response && <DirectionsRenderer directions={response} />}
-        </>
+        
+        <Marker position={center} />
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )}
+        
       </GoogleMap>
+      <div onClick={calculateRoute}
+        className="z-20  xl:w-96 mx-auto absolute right-2 bottom-20 mr-9  sm:justify-center flex"
+        style={{ transform: "traslateX(-50%)", margin: "auto" }}
+      >
+        <div className="justify-center text-center mt-20">
+          <div
+            style={{ cursor: "pointer" }}
+            className="  bg-white rounded-lg p-2"
+          >
+            {location}
+          </div>
+        </div>
+      </div>
       <Sidebar />
     </>
   ) : (
