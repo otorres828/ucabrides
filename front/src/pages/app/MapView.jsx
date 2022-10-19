b67508e36e9881433fb1be789e8647cde5da1c7a
Oliver Torres <olivertorres1997@gmail.com>
2022-10-19 01:08:36 -0400

    problema resuelto2

--- front/src/pages/app/MapView.jsx
+++ front/src/pages/app/MapView.jsx
@@ -1,16 +1,17 @@
-
 import React, { useState } from "react";
+import Sidebar from "../../components/app/Sidebar";
+import logo from "../../logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   Marker,
   GoogleMap,
   useJsApiLoader,
   DirectionsRenderer,
 } from "@react-google-maps/api";
-import Sidebar from "../../components/app/Sidebar";
-import logo from "../../logo.svg";
-// import BotonMiUbicacion from "../../components/app/BotonMiUbicacion";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationCrosshairs,faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faLocationCrosshairs,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 
 const location = <FontAwesomeIcon icon={faLocationCrosshairs} />;
 const clear = <FontAwesomeIcon icon={faTrash} />;
@@ -31,8 +32,8 @@ const destino = {
 };
 
 function MapView() {
-  const [map, setMap] = useState(/** @type google.maps.Map */ (null))
-  const [directionsResponse, setDirectionsResponse] = useState('');
+  const [map, setMap] = useState(null);
+  const [directionsResponse, setDirectionsResponse] = useState("");
   const [my_location, setMy_location] = useState(null);
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
@@ -40,33 +41,32 @@ function MapView() {
   });
 
   function obtener_mi_ubicacion() {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        setMy_location({
-          lat:position.coords.latitude,
-          lng:position.coords.longitude
-        })
-        map.panTo(my_location)
+    const google=window.google;
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setMy_location({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
       });
-    }
+      map.panTo(new google.maps.LatLng( position.coords.latitude,  position.coords.longitude) )
+    });
+  }
 
   async function calculateRoute() {
     const google = window.google;
-    const directionsService = new google.maps.DirectionsService()
+    const directionsService = new google.maps.DirectionsService();
     const results = await directionsService.route({
       origin: center,
       destination: destino,
       travelMode: google.maps.TravelMode.DRIVING,
-    })
-    setDirectionsResponse(results)
-
+    });
+    setDirectionsResponse(results);
   }
 
   function limpiar_ruta() {
-    setDirectionsResponse(null)
+    setDirectionsResponse(null);
     setMy_location(null);
-    map.panTo(center)
+    map.panTo(center);
   }
-
 
   return isLoaded ? (
     <>
@@ -75,22 +75,22 @@ function MapView() {
         center={center}
         zoom={16}
         options={{
-            zoomControl: false,
-            streetViewControl: false,
-            mapTypeControl: false,
-            fullscreenControl: false,
-          }}
-          onLoad={map => setMap(map)}
+          zoomControl: false,
+          streetViewControl: false,
+          mapTypeControl: false,
+          fullscreenControl: false,
+        }}
+        onLoad={(map) => setMap(map)}
       >
-        <Marker position={center} onClick={calculateRoute}/>
+        <Marker position={center} onClick={calculateRoute} />
 
         {my_location && <Marker position={my_location} />}
-          {directionsResponse && (
-            <DirectionsRenderer directions={directionsResponse} />
-          )}
-        
+        {directionsResponse && (
+          <DirectionsRenderer directions={directionsResponse} />
+        )}
       </GoogleMap>
-      <div onClick={obtener_mi_ubicacion}
+      <div
+        onClick={obtener_mi_ubicacion}
         className="z-20  xl:w-96 mx-auto absolute right-2 bottom-20 mr-9  sm:justify-center flex"
         style={{ transform: "traslateX(-50%)", margin: "auto" }}
       >
@@ -104,8 +104,9 @@ function MapView() {
         </div>
       </div>
 
-      {(directionsResponse || my_location) &&
-        <div onClick={limpiar_ruta}
+      {(directionsResponse || my_location) && (
+        <div
+          onClick={limpiar_ruta}
           className="z-20  xl:w-96 mx-auto absolute left-2 bottom-20 mr-9  sm:justify-center flex"
           style={{ transform: "traslateX(-50%)", margin: "auto" }}
         >
@@ -118,7 +119,7 @@ function MapView() {
             </div>
           </div>
         </div>
-      }
+      )}
       <Sidebar />
     </>
   ) : (
