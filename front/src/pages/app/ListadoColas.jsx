962ebb6ac59f0cc07ffb30735739359450f10af0
Oliver Torres <olivertorres1997@gmail.com>
2022-12-01 17:39:45 -0400

    listado

--- front/src/pages/app/ListadoColas.jsx
+++ front/src/pages/app/ListadoColas.jsx
@@ -36,7 +36,7 @@ function ListadoColas() {
   return rutas.length > 0 && distancia > 0 && JSON.stringify(localizacion_usuario)!=='{}' ? (
     <>
       <div className="container mx-auto">
-        <div className="p-5 pt-12 mb-10">
+        <div className="p-5 pt-12 mb-10 sm:px-20">
           <h1 className="font-bold text-slate-600 text-xl">Bienvenido al listado de Colas Disponibles</h1>
           <BasicTable rutas={rutas} localizacion_usuario={localizacion_usuario} distancia={distancia} />
         </div>
