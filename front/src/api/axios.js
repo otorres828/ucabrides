c0b0fc846a6c7ac56c95b4eed822aea7cae07e00
Oliver Torres <olivertorres1997@gmail.com>
2022-12-12 01:32:09 -0400

    cambio back hosting

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL: 'http://127.0.0.1:8000/api'
-    baseURL: 'https://api-ucabrides.herokuapp.com/api'
+    baseURL: 'https://ucabrides-api-sjd77.ondigitalocean.app/api'
 });
