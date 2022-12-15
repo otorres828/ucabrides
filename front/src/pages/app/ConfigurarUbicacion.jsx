d0885658c3e9fb300893769fd8cfa616e608dd3e
Oliver Torres <olivertorres1997@gmail.com>
2022-12-15 04:27:43 -0400

    ubicacion

--- front/src/pages/app/ConfigurarUbicacion.jsx
+++ front/src/pages/app/ConfigurarUbicacion.jsx
@@ -97,7 +97,7 @@ const PlacesAutocomplete = ({ setSelected }) => {
         onChange={(e) => setValue(e.target.value)}
         disabled={!ready}
         placeholder="Escriba su direccion"
-        className="shadow-lg bg-blue-100 w-full p-3"
+        className="bg-gray-50 border border-gray-300 text-gray-700 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       />
 
       {status === "OK" &&
