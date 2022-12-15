697d4539eae70a7c3e0d33734b67d6a249973e1c
Oliver Torres <olivertorres1997@gmail.com>
2022-12-15 04:39:06 -0400

    poronga

--- front/src/components/app/DistanciaCaminar.jsx
+++ front/src/components/app/DistanciaCaminar.jsx
@@ -69,7 +69,7 @@ function DistanciaCaminar() {
         onClick={() => {
           setOpen(true);
         }}
-        className="cursor-pointer w-full  border-gray-100 text-gray-50 hover:text-gray-600 py-4 pl-6 pr-3  block hover:bg-gray-100 transition duration-150"
+        className="cursor-pointer w-full border-t border-gray-100  text-gray-50 hover:text-gray-600 py-4 pl-6 pr-3  block hover:bg-gray-100 transition duration-150"
       >
         <img
           src="https://avatars0.githubusercontent.com/u/35900628?v=4"
