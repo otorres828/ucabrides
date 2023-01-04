87aa5f9adda5df9398cb58bdd4c2e8daf1d157ac
Oliver Torres <olivertorres1997@gmail.com>
2023-01-04 16:34:20 -0400

    quitar bandera

--- front/src/components/app/darCola/listadoConductor.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const listadoConductor = () => {
-    return ( <h1>Imprimiendo listado de colas</h1> );
-}
- 
-export default listadoConductor;
--- front/src/pages/app/DarCola/ListadoRutas.jsx
+++ front/src/pages/app/DarCola/ListadoRutas.jsx
@@ -24,26 +24,26 @@ function ListadoRutas({ access_token }) {
   const [vehiculos, setVehiculos] = React.useState(null);
   const refresh = () => window.location.reload(true);
 
-  const obtenerRutas =() => {
+  const obtenerRutas = () => {
     axios
-      .get("rutas",{
+      .get("rutas", {
         headers: {
           Authorization: `Bearer ${access_token}`,
           Accept: "application/json",
         },
-      }).then((res)=>{
+      })
+      .then((res) => {
         setRutas(res.data);
-      }) 
+      });
   };
 
   const obtenerOrdenes = () => {
     axios
-      .get("ordenes_rutas",
-      {
+      .get("ordenes_rutas", {
         headers: {
           Authorization: `Bearer ${access_token}`,
           Accept: "application/json",
-        }
+        },
       })
       .then((response) => {
         setOrdenes(response.data);
@@ -51,52 +51,58 @@ function ListadoRutas({ access_token }) {
   };
 
   const obtenerVehiculos = async () => {
-    const { data } = await axios.get("vehiculos", {headers: {Authorization: `Bearer ${access_token}`, Accept: "application/json", }, });
+    const { data } = await axios.get("vehiculos", {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        Accept: "application/json",
+      },
+    });
     setVehiculos(data);
   };
 
-  const handleOnChange = (position) => { 
-    if (rutas!==null){
+  const handleOnChange = (position) => {
+    if (rutas !== null) {
       var ban = false;
       rutas.map(function (ruta) {
-          if(ruta.estatus===true)
-             ban=true
-      })
-     
-      if(!ban){
+        if (ruta.estatus === true) ban = true;
+      });
+
+      if (!ban) {
         setRutaseleccionada(rutas[position]);
         setDataRuta(rutaseleccionada);
         //Aqui abrimos el modal para seleccionar los vehiculos
         setmodalHandler(true);
-        
-      }else{
-        enqueueSnackbar('Ya tiene una ruta activa',{ variant: "error" })
+      } else {
+        enqueueSnackbar("Ya tiene una ruta activa", { variant: "error" });
       }
     }
   };
-  
+
   const desactivarRuta = async (id) => {
     var iddeorden;
-    if(ordenes.usuarios.lenght>0){
-      enqueueSnackbar('Esta ruta tiene usuarios asignados, debes de cancelarla o completarla',{ variant: "error" })
-    }else{
+    if (ordenes.usuarios.lenght > 0) {
+      enqueueSnackbar(
+        "Esta ruta tiene usuarios asignados, debes de cancelarla o completarla",
+        { variant: "error" }
+      );
+    } else {
       //DESACTIVAR ORDEN Y RUTA
-      enqueueSnackbar('Ruta desactivada con exito',{ variant: "success" })
+      enqueueSnackbar("Ruta desactivada con exito", { variant: "success" });
       iddeorden = ordenes._id;
       const r = await axios.post(
         `desactivar`,
-        { orden_ruta_id:iddeorden,ruta_id:id},
+        { orden_ruta_id: iddeorden, ruta_id: id },
         {
           headers: {
-                Authorization: `Bearer ${access_token}`,
-                Accept: "application/json",
-              },
-            }
-            );
+            Authorization: `Bearer ${access_token}`,
+            Accept: "application/json",
+          },
         }
-        refresh();
-      };
-      
+      );
+    }
+    refresh();
+  };
+
   const sethandleFalse = () => {
     const buscando = checkedState.map((item, index) => {
       return false;
@@ -107,10 +113,10 @@ function ListadoRutas({ access_token }) {
   React.useEffect(() => {
     obtenerRutas();
     obtenerOrdenes();
-    obtenerVehiculos()
+    obtenerVehiculos();
   }, []);
 
-  return  detalleOrden === false ? (
+  return (
     <>
       <div className="mx-auto my-12 pb-12 vh-100">
         <div className="bg-gray-100 relative shadow rounded-lg w-5/6 md:w-4/6  lg:w-3/6 xl:w-2/6 mx-auto">
@@ -118,58 +124,56 @@ function ListadoRutas({ access_token }) {
             <h1 className="text-left block pt-5 pb-2 font-bold  text-xl sm:text-3xl text-gray-900">
               Listado de Rutas
             </h1>
-            <div
-        className="bg-blue-600 m-2.5 p-2 rounded-lg circle cursor-pointer"
-      >
-        ➕
-      </div>
+            <div className="bg-blue-600 m-2.5 p-2 rounded-lg circle cursor-pointer">
+              ➕
             </div>
-            <div className="flex justify-between items-center my-5 px-6">
+          </div>
+          <div className="flex justify-between items-center my-5 px-6">
             {rutas === null ? (
               "Cargando..."
             ) : (
-            <TableContainer>
-              <Table >
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Nombre</TableCell>
-                    <TableCell align="right">Estatus</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rutas.map((ruta, index) => 
-                   
-                   ( <TableRow
-                      key={ruta._id}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >  
-                      <TableCell>{ruta.nombre}</TableCell>
-                      <TableCell align="right">
-                        <input
-                          type="checkbox"
-                          id={ruta._id}
-                          name={ruta.nombre}
-                          value={ruta._id}
-                          checked={ruta.estatus}
-                          onChange={
-                            ruta.estatus === false
-                              ? () => handleOnChange(index)
-                              : () => {
-                                  desactivarRuta(ruta._id);
-                                }
-                          }
-                        />
-                      </TableCell>
-                    </TableRow>)
-                    
-                  )}
-                </TableBody>
-              </Table>
-            </TableContainer>
+              <TableContainer>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Nombre</TableCell>
+                      <TableCell align="right">Estatus</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rutas.map((ruta, index) => (
+                      <TableRow
+                        key={ruta._id}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell>{ruta.nombre}</TableCell>
+                        <TableCell align="right">
+                          <input
+                            type="checkbox"
+                            id={ruta._id}
+                            name={ruta.nombre}
+                            value={ruta._id}
+                            checked={ruta.estatus}
+                            onChange={
+                              ruta.estatus === false
+                                ? () => handleOnChange(index)
+                                : () => {
+                                    desactivarRuta(ruta._id);
+                                  }
+                            }
+                          />
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
             )}
-            </div>
-          
-          {modalHandler && vehiculos &&
+          </div>
+
+          {modalHandler && vehiculos && (
             <ActivarRuta
               cambiarModal={() => setmodalHandler(false)}
               setFalse={() => sethandleFalse()}
@@ -178,16 +182,11 @@ function ListadoRutas({ access_token }) {
               access_token={access_token}
               vehiculos={vehiculos}
             />
-          }
-
+          )}
         </div>
       </div>
       <Rsidebar />
     </>
-  ) : detalleOrden === false ? (
-   <div></div>
-  ) : (
-   <></>
   );
 }
 
