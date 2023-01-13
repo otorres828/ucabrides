755ad6615f638bee1db01d1de3b3be18b64a84f0
Oliver Torres <olivertorres1997@gmail.com>
2023-01-13 01:50:14 -0400

    landding

--- front/src/components/web/Team.jsx
+++ front/src/components/web/Team.jsx
@@ -4,6 +4,8 @@ import Luis from "../../images/team/luis.jpeg";
 import Wladimir from "../../images/team/wladimir.jpg";
 import Fady from "../../images/team/fady.jpg";
 import Lorenzo from "../../images/team/lorenzo.jpg";
+import sotillo from "../../images/team/sotillo.jpeg";
+import rafael from "../../images/team/rafael.jpeg";
 import { Navigation } from "swiper";
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -68,85 +70,16 @@ function Team() {
             <p className="text-gray-600 text-center">Developer</p>
           </div>
         </SwiperSlide>
-        <SwiperSlide>
-          <div className=" relative flex flex-col items-center p-6 bg-slate-50 rounded shadow-xl">
-            <svg
-              className="w-16 h-16 p-1 -mt-1 mb-2"
-              viewBox="0 0 64 64"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g fill="none" fillRule="evenodd">
-                <rect
-                  className="fill-current text-blue-600"
-                  width="64"
-                  height="64"
-                  rx="32"
-                />
-                <g strokeWidth="2" transform="translate(19.429 20.571)">
-                  <circle
-                    className="stroke-current text-white"
-                    strokeLinecap="square"
-                    cx="12.571"
-                    cy="12.571"
-                    r="1.143"
-                  />
-                  <path
-                    className="stroke-current text-white"
-                    d="M19.153 23.267c3.59-2.213 5.99-6.169 5.99-10.696C25.143 5.63 19.514 0 12.57 0 5.63 0 0 5.629 0 12.571c0 4.527 2.4 8.483 5.99 10.696"
-                  />
-                  <path
-                    className="stroke-current text-blue-300"
-                    d="M16.161 18.406a6.848 6.848 0 003.268-5.835 6.857 6.857 0 00-6.858-6.857 6.857 6.857 0 00-6.857 6.857 6.848 6.848 0 003.268 5.835"
-                  />
-                </g>
-              </g>
-            </svg>
-            <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">
-              Cesar Sotillo
-            </h4>
-            <p className="text-gray-600 text-center">Database</p>
-          </div>
-        </SwiperSlide>
+  
     
         <SwiperSlide>
           <div className=" relative flex flex-col items-center p-6 bg-slate-50 rounded shadow-xl">
-            <svg
-              className="w-16 h-16 p-1 -mt-1 mb-2"
-              viewBox="0 0 64 64"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g fill="none" fillRule="evenodd">
-                <rect
-                  className="fill-current text-blue-600"
-                  width="64"
-                  height="64"
-                  rx="32"
-                />
-                <g transform="translate(22.857 19.429)" strokeWidth="2">
-                  <path
-                    className="stroke-current text-white"
-                    strokeLinecap="square"
-                    d="M12.571 4.571V0H0v25.143h12.571V20.57"
-                  />
-                  <path
-                    className="stroke-current text-white"
-                    d="M16 12.571h8"
-                  />
-                  <path
-                    className="stroke-current text-white"
-                    strokeLinecap="square"
-                    d="M19.429 8L24 12.571l-4.571 4.572"
-                  />
-                  <circle
-                    className="stroke-current text-blue-300"
-                    strokeLinecap="square"
-                    cx="12.571"
-                    cy="12.571"
-                    r="3.429"
-                  />
-                </g>
-              </g>
-            </svg>
+          <img
+              src={rafael}
+              width="96"
+              height="96"
+              className="rounded-full mx-auto shadow-md border-2 border-white transition duration-200 transform hover:scale-110"
+            />
             <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">
               Rafael Muñoz
             </h4>
@@ -181,7 +114,22 @@ function Team() {
             <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">
               Wladimir SanVicente
             </h4>
-            <p className="text-gray-600 text-center">UI</p>
+            <p className="text-gray-600 text-center">UI / Documenter</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" relative flex flex-col items-center p-6 bg-slate-50 rounded shadow-xl">
+          <img
+              className="rounded-full"
+              src={sotillo}
+              width="96"
+              height="96"
+              alt="Testimonial 01"
+            />
+            <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">
+              Cesar Sotillo
+            </h4>
+            <p className="text-gray-600 text-center">Database / Documenter</p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
